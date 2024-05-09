644716a36194c05e5d94a810c59fadf12cae9789
servatj <jservatlorca@gmail.com>
2024-05-09 23:59:52 +0400

    fix build

--- tests/e2e/healthcheck.e2e.test.ts
+++ tests/e2e/healthcheck.e2e.test.ts
@@ -3,7 +3,7 @@ import request from "supertest";
 
 import app from "@src/app";
 
-describe.only("auth", () => {
+describe("auth", () => {
   beforeAll(async () => {
     nock.disableNetConnect();
     nock.enableNetConnect("127.0.0.1");
