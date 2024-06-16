3ab5812feb18e9d597ea668e8a39fc1572b4b095
servatj <jservatlorca@gmail.com>
2024-06-16 21:12:09 +0400

    add login test

--- tests/e2e/auth-controller.spec.ts
+++ tests/e2e/auth-controller.spec.ts
@@ -7,11 +7,25 @@ test("register user successfully", async ({ request }) => {
   const response = await request.post(`${baseURL}/register`, {
     data: {
       name: "John Doe",
-      email: "john.doe@example.com",
-      password: "password123",
+      email: "john.doe1@example.com",
+      password: "password1234",
     },
   });
   expect(response.status()).toBe(200);
   const responseBody = await response.json();
   expect(responseBody).toEqual({ message: "User registered successfully" });
+});
+
+test("Login User", async ({ request }) => {
+  const baseURL = "http://localhost:4000";
+  const response = await request.post(`${baseURL}/login`, {
+    data: {
+      name: "John Doe",
+      email: "john.doe1@example.com",
+      password: "password1234",
+    },
+  });
+  expect(response.status()).toBe(200);
+  const responseBody = await response.json();
+  expect(responseBody).toHaveProperty("token");
 });
